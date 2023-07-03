39d4a566dcbfb7a785eb49a4362af043d03c8447
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-03 17:20:17 +0900

    :lipstick: Home 페이지 ui 수정
    home 페이지 ui 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,11 @@ import Layout from "../libs/components/Layout";
 const Home: NextPage = () => {
   return (
     <Layout title="홈" hasTabBar>
-      <div className="flex flex-col space-y-5 ">
+      <div className="flex flex-col space-y-5 divide-y">
         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
           <div
             key={i}
-            className="flex justify-between px-4 pb-5 border-b cursor-pointer"
+            className="flex justify-between px-4 pt-5 cursor-pointer"
           >
             <div className="flex space-x-4">
               <div className="w-20 h-20 bg-gray-400 rounded-md" />
