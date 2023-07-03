9565e0a3d123341d4773616030270c64d3de2dbd
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-03 18:29:29 +0900

    :recycle: Live 페이지 수정
    live 페이지 수정

--- src/pages/live/index.tsx
+++ src/pages/live/index.tsx
@@ -1,19 +1,21 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import Layout from "../../libs/components/Layout";
+import FloatingButton from "../../libs/components/FloatingButton";
 
 const Live: NextPage = () => {
   return (
     <Layout hasTabBar title="라이브">
       <div className=" divide-y-[1px] space-y-4">
         {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <div className="px-4 pt-4" key={i}>
+          <Link key={i} href={`/live/${i}`} className="block px-4 pt-4">
             <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
             <h1 className="mt-2 text-2xl font-bold text-gray-900">
               Galaxy S50
             </h1>
-          </div>
+          </Link>
         ))}
-        <button className="fixed p-4 text-white transition-colors bg-orange-400 border-transparent rounded-full shadow-xl cursor-pointer hover:bg-orange-500 bottom-24 right-5">
+        <FloatingButton href="/live/create">
           <svg
             className="w-6 h-6"
             fill="none"
@@ -28,7 +30,7 @@ const Live: NextPage = () => {
               d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
             ></path>
           </svg>
-        </button>
+        </FloatingButton>
       </div>
     </Layout>
   );
