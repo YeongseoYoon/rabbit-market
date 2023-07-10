b5f352730848ec0b2225bc38e024c56d17e49c9f
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-10 14:03:44 +0900

    :card_file_box: Token 테이블 추가
    token 테이블 추가

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -3,6 +3,19 @@ import withHandler from "@/libs/server/withHandler";
 import client from "@/libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   return res.status(200).end();
 }
 
