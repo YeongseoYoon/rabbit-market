ce57e6c99d70ff5258adc3d23899e9e9a8bcee7f
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-10 18:21:35 +0900

    :recycle: UseMutation 훅 변경
    useMutation 훅 변경

--- src/libs/client/useMutation.tsx
+++ src/libs/client/useMutation.tsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
-
-interface UseMutationState {
+interface UseMutationState<T> {
   loading: boolean;
-  data?: object;
+  data?: T;
   error?: object;
 }
-type UseMutationResult = [(data: any) => void, UseMutationState];
-
-export default function useMutation(url: string): UseMutationResult {
-  const [state, setSate] = useState<UseMutationState>({
+type UseMutationResult<T> = [(data: any) => void, UseMutationState<T>];
+export default function useMutation<T = any>(
+  url: string
+): UseMutationResult<T> {
+  const [state, setState] = useState<UseMutationState<T>>({
     loading: false,
     data: undefined,
     error: undefined,
   });
   function mutation(data: any) {
-    setSate((prev) => ({ ...prev, loading: true }));
+    setState((prev) => ({ ...prev, loading: true }));
     fetch(url, {
       method: "POST",
       headers: {
@@ -23,9 +23,9 @@ export default function useMutation(url: string): UseMutationResult {
       body: JSON.stringify(data),
     })
       .then((response) => response.json().catch(() => {}))
-      .then((data) => setSate((prev) => ({ ...prev, data })))
-      .catch((error) => setSate((prev) => ({ ...prev, error })))
-      .finally(() => setSate((prev) => ({ ...prev, loading: false })));
+      .then((data) => setState((prev) => ({ ...prev, data })))
+      .catch((error) => setState((prev) => ({ ...prev, error })))
+      .finally(() => setState((prev) => ({ ...prev, loading: false })));
   }
   return [mutation, { ...state }];
 }
