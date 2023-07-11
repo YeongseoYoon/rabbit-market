98a4ecd8fddebae279c2c5206255e5958b6343bc
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-11 10:19:53 +0900

    :sparkles: Session 모듈 분리
    session 모듈 분리

--- src/pages/api/users/confirm.tsx
+++ src/pages/api/users/confirm.tsx
@@ -1,36 +1,29 @@
-import { withIronSessionApiRoute } from "iron-session/next";
+import { withApiSession } from "@/libs/server/withSession";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import client from "@/libs/server/client";
-
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  const exists = await client.token.findUnique({
+  const foundToken = await client.token.findUnique({
     where: {
       payload: token,
     },
     include: { user: true },
   });
-  if (!exists) return res.status(404).end();
+  if (!foundToken) return res.status(404).end();
   req.session.user = {
-    id: exists.userId,
+    id: foundToken.userId,
   };
   await req.session.save();
-  res.status(200).end();
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId,
+    },
+  });
+  res.json({ ok: true });
 }
-export default withIronSessionApiRoute(withHandler("POST", handler), {
-  cookieName: "carrotsession",
-  password:
-    "98fwfwffwter88797752343;ldfksjgdsflgjdfklgjdf5435345345345lgjflkgjdgd",
-});
+export default withApiSession(withHandler("POST", handler));
