48975b964968340848c4f7cc2f4676f2286af209
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-11 10:21:34 +0900

    :recycle: Session 모듈 분리로 인한 api 엔드포인트 수정
    session 모듈 분리로 인한 api 엔드포인트 수정

--- src/pages/api/users/me.tsx
+++ src/pages/api/users/me.tsx
@@ -1,32 +1,19 @@
-import { withIronSessionApiRoute } from "iron-session/next";
+import { withApiSession } from "@/libs/server/withSession";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import client from "@/libs/server/client";
-
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  console.log(req.session.user);
-  /*const profile = await client.user.findUnique({
+  const profile = await client.user.findUnique({
     where: { id: req.session.user?.id },
   });
   res.json({
     ok: true,
     profile,
-  });*/
+  });
 }
 
-export default withIronSessionApiRoute(withHandler("GET", handler), {
-  cookieName: "carrotsession",
-  password:
-    "98fwfwffwter88797752343;ldfksjgdsflgjdfklgjdf5435345345345lgjflkgjdgd",
-});
+export default withApiSession(withHandler("GET", handler));
