ccfcaabea11ed0568c94fa91d0510747f09b7fed
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-15 09:31:23 +0900

    :recycle: 메인 홈 데이터 로드
    메인 홈 데이터 로드

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,32 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import useSWR from "swr";
+import { Product } from "@prisma/client";
+
 import Layout from "@/libs/components/Layout";
 import FloatingButton from "@/libs/components/FloatingButton";
 import Item from "@/libs/components/Item";
 import useUser from "@/libs/client/useUser";
-import Head from "next/head";
 
+interface ProductsResponse {
+  ok: boolean;
+  products: Product[];
+}
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
+  const { data } = useSWR<ProductsResponse>("/api/products");
   return (
     <Layout title="홈" hasTabBar>
       <Head>
         <title>Home</title>
       </Head>
       <div className="flex flex-col space-y-5 divide-y">
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        {data?.products?.map((product) => (
           <Item
-            id={i}
-            key={i}
-            title="iPhone 14"
-            price={99}
+            id={product.id}
+            key={product.id}
+            title={product.name}
+            price={product.price}
             comments={1}
             hearts={1}
           />
